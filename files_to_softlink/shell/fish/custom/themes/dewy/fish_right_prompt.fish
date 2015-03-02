ccaf52a06bc617662e609da9d57548ee152fc5d6
nakal <nakal@users.noreply.github.com>
2015-03-02 12:42:11 +0100

    Fix for git "ahead" prompt

--- files_to_softlink/shell/fish/custom/themes/dewy/fish_right_prompt.fish
+++ files_to_softlink/shell/fish/custom/themes/dewy/fish_right_prompt.fish
@@ -13,7 +13,7 @@ function get_git_status -d "Git status on the right side"
 			if test "$reposize" -lt "$__git_status_slow_repo_size"
 				set -l dirty (command git status -s -uno --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
 				set -l uncommitted (command git status -s --ignore-submodules=dirty | egrep "^\?\? " | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-				set -l unpushed (command git status -sb --ignore-submodules=dirty | head -1 | grep '\[ahead ' | sed 's/.*\[ahead \([0-9][0-9]*\)\]$/\1/' 2> /dev/null)
+				set -l unpushed (command git status -sb --ignore-submodules=dirty | head -1 | grep '\[ahead ' | sed 's/.*\[ahead \([0-9][0-9]*\).*/\1/' 2> /dev/null)
 				if [ "$dirty$uncommitted" != "00" ]
 					set_color -b normal
 					if [ "$dirty" != "0" ]
