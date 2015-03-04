af96dc9855d46200dbbe43a9c521da5b53b77835
nakal <nakal@users.noreply.github.com>
2015-03-04 17:21:33 +0100

    Split in shell-only and X initialization

--- files_to_softlink/bootstrap.sh
+++ files_to_softlink/bootstrap.sh
@@ -1,32 +1,25 @@
 #!/bin/sh
 
-# Git repo helper
-git_init_dir()
-{
-	REPOPATH="$1"
-	REPODIR="$2"
-	test -d "$REPODIR" && git pull
-	test -d "$REPODIR" || git clone "$REPOPATH" "$REPODIR"
-}
+echo Preparing shell-based access...
+THISDIR=`dirname $0`
+$THISDIR/bootstrap_shell_only.sh
 
 echo Checking packages...
-pkg info slim sudo gtk2 vim git xterm xscreensaver \
+pkg info slim sudo gtk2 xterm xscreensaver \
 	hs-xmonad hs-network hs-xmonad-contrib \
-	firefox gimp libreoffice dmenu gmrun tmux \
+	firefox gimp libreoffice dmenu gmrun \
 	dzen2 weechat-devel zenity claws-mail \
 	gtk-oxygen-engine xrdb xsetroot setxkbmap gnupg \
-	xmodmap hsetroot fish \
+	xmodmap hsetroot \
 	> /dev/null
 if [ $? -ne 0 ]; then
-	echo ERROR: Missing packages for bootstrap.
+	echo "ERROR: Missing packages for bootstrap (for X)."
 	exit 1
 fi
 
 cd $HOME
-REMOVE_FILES=".cshrc .xinitrc .Xdefaults .gtkrc-2.0 .tmux.conf .indent.pro \
-	$HOME/.config/gtk-3.0/settings.ini .gitignore_global .gitconfig \
-	$HOME/.config/fish/config.fish $HOME/.config/fish/custom \
-	$HOME/.config/fish/functions \
+REMOVE_FILES=".xinitrc .Xdefaults .gtkrc-2.0 \
+	$HOME/.config/gtk-3.0/settings.ini \
 	"
 
 for df in $REMOVE_FILES; do
@@ -39,55 +32,18 @@ done
 cd $HOME
 echo Removing old softlinks...
 rm -f $REMOVE_FILES
-echo Moving vim configuration out of the way...
-mv .vim .vim-bak-`date +%s` 2> /dev/null
-mv .vimrc .vimrc-bak-`date +%s` 2> /dev/null
 
 # prepare conf in user's home
 echo Reinstalling softlinks...
-ln -s ~/.xmonad/files_to_softlink/shell/tcsh/.cshrc .
 ln -s ~/.xmonad/files_to_softlink/xsettings/.xinitrc .
 ln -s ~/.xmonad/files_to_softlink/xsettings/.Xdefaults .
 ln -s ~/.xmonad/files_to_softlink/xsettings/.gtkrc-2.0 .
-ln -s ~/.xmonad/files_to_softlink/tmux/.tmux.conf .
-ln -s ~/.xmonad/files_to_softlink/misc/.indent.pro .
-ln -s ~/.xmonad/files_to_softlink/git/.gitignore_global .
-ln -s ~/.xmonad/files_to_softlink/git/.gitconfig .
 
 mkdir -p $HOME/.config/gtk-3.0
 cd $HOME/.config/gtk-3.0
 ln -s ~/.xmonad/files_to_softlink/xsettings/settings.ini .
 
-echo Preparing fish shell...
-mkdir -p $HOME/.config/fish
-cd $HOME/.config/fish
-ln -s ~/.xmonad/files_to_softlink/shell/fish/config.fish .
-ln -s ~/.xmonad/files_to_softlink/shell/fish/custom .
-ln -s ~/.xmonad/files_to_softlink/shell/fish/functions .
 cd $HOME
-git_init_dir https://github.com/bpinto/oh-my-fish.git .oh-my-fish
-
-echo Preparing vim and plugins...
-mkdir -p .vim/bundle .vim/autoload .vim/colors
-cd .vim
-ln -s ~/.xmonad/files_to_softlink/vim/vimrc .
-ln -s ~/.xmonad/files_to_softlink/vim/update-plugins.sh .
-cd autoload
-git clone https://github.com/tpope/vim-pathogen.git
-ln -s vim-pathogen/autoload/pathogen.vim .
-cd ../bundle
-git clone https://github.com/Shougo/unite.vim
-git clone https://github.com/tsukkee/unite-tag
-git clone https://github.com/tpope/vim-fugitive
-git clone https://github.com/tpope/vim-unimpaired
-git clone https://github.com/altercation/vim-colors-solarized
-git clone https://github.com/bling/vim-airline
-cd ../colors
-git clone https://github.com/gosukiwi/vim-atom-dark.git
-ln -s vim-atom-dark/colors/atom-dark.vim .
-ln -s vim-atom-dark/colors/atom-dark-256.vim .
-cd ../..
-
 echo Preparing xmonad...
 xmonad --recompile
 
