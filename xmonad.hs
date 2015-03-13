08a18fc04ef925a85f4e0c9defd184edfac4d70f
nakal <nakal@users.noreply.github.com>
2015-03-13 15:36:51 +0100

    Add tmux to vim sessions to avoid weirdness

--- xmonad.hs
+++ xmonad.hs
@@ -100,9 +100,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
 
     -- launch vim (in various ways, with most common uses)
-    , ((modm,			xK_v     ), spawn $ (XMonad.terminal conf) ++ " -e tcsh -c 'vim -c \"CtrlP\"'")
+    , ((modm,			xK_v     ), spawn $ (XMonad.terminal conf) ++ " -e tmux -2 new-session 'vim -c CtrlP'")
     , ((modm .|. shiftMask,	xK_v     ), spawn $ (XMonad.terminal conf) ++ " -e vim ~/.vim/vimrc")
-    , ((modm .|. controlMask,	xK_v     ), spawn $ "cd ~/src/nid-prototype ; " ++ (XMonad.terminal conf) ++ " -e tcsh -c 'vim -c \"CtrlP\"'")
+    , ((modm .|. controlMask,	xK_v     ), spawn $ "cd ~/src/nid-prototype ; " ++ (XMonad.terminal conf) ++ " -e tmux -2 new-session 'vim -c CtrlP'")
     , ((modm .|. shiftMask,	xK_x     ), spawn $ (XMonad.terminal conf) ++ " -e vim ~/.xmonad/xmonad.hs")
     , ((modm,	xK_i     ), spawn $ (XMonad.terminal conf) ++ " -title weechat -e sh -c 'tmux has-session -t weechat && tmux attach-session -d -t weechat || tmux -2 new-session -s weechat weechat'" )
 
