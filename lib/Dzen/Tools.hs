5444458cf6340237c4ac295e5d2ecaf9dc7f5489
nakal <nakal@users.noreply.github.com>
2015-03-16 17:11:13 +0100

    Inconsolata also for dzen2

--- lib/Dzen/Tools.hs
+++ lib/Dzen/Tools.hs
@@ -7,7 +7,7 @@ dzenBitmap :: FilePath -> String -> String
 dzenBitmap homedir pic = homedir ++ "/.xmonad/dzen2/" ++ pic ++ ".xbm"
 
 dzenFont :: String
-dzenFont = "Bitstream Vera Sans Mono:size=11:bold"
+dzenFont = "Inconsolata:size=12:bold"
 
 dzenExec :: String
 dzenExec = "dzen2 -e '' -y 0 -h 24 -fg '#FFFFFF' -bg '#202020' -fn '" ++ dzenFont ++ "'"
