f630aec208e7c25cf580899280e85f961d466aa0
nakal <nakal@users.noreply.github.com>
2016-08-31 15:58:16 +0200

    Stop setting terminal type

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+MYDIR=`dirname $0`
+. $MYDIR/include.sh
+
 if [ -z "$@" ]; then
-	exec xterm -class "xmDialog" -e $0 x
+	exec "$TERMINAL" "$CLASS_OPT" "xmDialog" -e $0 x
 fi
 
 dialog --yesno "Reboot?" 5 40 || exit 0
