ffdd667e32ba00936827a9ee063e8b365bbbe81e
nakal <nakal@users.noreply.github.com>
2020-05-16 19:54:55 +0200

    Prefer Libreoffice over OpenOffice

--- scripts/office.sh
+++ scripts/office.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-office_bin=`ls /usr/local/bin/openoffice-*[0-9] 2> /dev/null`
+office_bin=`ls /usr/local/bin/libreoffice* 2> /dev/null`
 if [ ! -x "$office_bin" ]; then
-	office_bin=$(ls /usr/local/bin/libreoffice* /usr/bin/libreoffice* 2> /dev/null | sort | head -n 1)
+	office_bin=$(ls ls /usr/local/bin/openoffice-*[0-9] 2> /dev/null | sort | head -n 1)
 fi
 
 exec $office_bin $@
